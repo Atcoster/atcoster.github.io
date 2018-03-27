9d38f0bd8d39801454073fd07dd08ae5abca2019
At Coster <atcoster@gmail.com>
2018-03-27 17:47:05 +0200

    Create the Header component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import Logo from '../partials/Logo';
 import Navigation from '../partials/Navigation';
 import Social from '../partials/Social';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
+
+	constructor( props ) {
+		super( props );
+	}
+
+	handleNavClick( e ) {
+		this.props.handleNavClick( e );
+	}
 
 	render() {
 		return (
 			<aside className='header'>
 				<Logo />
-				<Navigation />
+				<Navigation handleNavClick= { this.handleNavClick.bind( this ) } />
 				<Social />
 			</aside>
 		);
 	}
+}
+
+Header.propTypes = {
+	handleNavClick : PropTypes.func.isRequired
 }
 
 export default Header;
